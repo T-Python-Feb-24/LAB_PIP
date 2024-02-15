e6bf82d65b5d984708ce3b878060b4023f4827bc
Salemhu <salem.hussain.alsalem@gmail.com>
2024-02-15 11:58:21 +0300

    I found a way to merge the two.

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from art import *
 from colorama import Fore, Back, Style
 FONT_COUNTER
 
-tprint("BELIEVE AND ACHEIVE",font="block")
+art1 = text2art("BELIEVE AND ACHEIVE",font="block")
 
-tprint("HELLO",font="sub-zero")
+art2 = text2art("HELLO",font="sub-zero")
 
-tprint("I'm Salem",font="3d")
+art3 = text2art("I'm Salem",font="3d")
 
-tprint("Good morning",font="cola")
+art4 = text2art("Good morning",font="cola")
 
-print(Fore.RED +"Hello world")
+print(Fore.RED + art1)
+print(Fore.BLUE + art2)
+print(Fore.CYAN + art3)
+print(Fore.GREEN + art4)
