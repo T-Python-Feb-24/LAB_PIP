99197ed310e6de141518118ab14cdff895d2b6aa
Shekah <shekhahathlol@gmail.com>
2024-02-15 13:31:44 +0300

    lab pip + Bonus

---
+++ Bonus.py
@@ -0,0 +1,8 @@
+# - Come up with different phrases with different art and decoration
+# - Use colorama to color the text in the terminal.
+from art import *
+from colorama import Fore, Back
+x = Fore.CYAN
+print(decor("barcode1")*10)
+print(x,text2art("python bootcamp"))
+print(decor("barcode1")*10)
---
+++ lab_pip.py
@@ -0,0 +1,6 @@
+from art import *
+
+one = tprint("BELIEVE AND ACHEIVE",font='block',chr_ignore=True)
+two = tprint("HELLO", font='sub-zero')
+print(one)
+print (two)
