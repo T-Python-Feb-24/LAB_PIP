67ecc600f4d9d2570942970ac7a18d45e76cbf6b
rahaf <rahaf09876509@gmail.com>
2024-02-15 14:46:41 +0300

    Done

---
+++ Text_art/main.py
@@ -0,0 +1,16 @@
+from art import *
+from colorama import Fore, Back, Style, init
+
+print(Fore.GREEN)
+tprint('''BELIEVE AND ACHEIVE
+''',font='block',chr_ignore=True)
+
+print(Fore.CYAN)
+tprint("HELLO", font="`sub-zero`")
+
+print(Fore.MAGENTA)
+init(autoreset=False)
+tprint('''WELCOME 
+TO 
+PYTHON 
+BOOTCMP''')
