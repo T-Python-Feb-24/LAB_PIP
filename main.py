e1f0c51ca4db09dce0eed25156d478dbf9236821
waad <waad.f.alanzi@gmail.com>
2024-02-15 14:14:23 +0300

    lab solved

---
+++ main.py
@@ -0,0 +1,49 @@
+from colorama import *
+from art import *
+
+
+a=text2art("BAA" , font='block', chr_ignore=True) #BELIEVE AND ACHEIVE
+print(a)
+print(Fore.LIGHTCYAN_EX)
+tprint("HELLO" , font='sub-zero')
+print(Fore.LIGHTRED_EX)
+tprint("Hi","rnd-xlarge")
+print(Fore.LIGHTMAGENTA_EX)
+print(decor("rand",both=True)*5)
+print("\n")
+
+print(Fore.LIGHTRED_EX)
+print("-------Take your coffee today ",art("coffee"),"-------\n\n")
+print(Fore.LIGHTMAGENTA_EX)
+print(decor("rand",both=True)*5)
+
+
+tprint("HAVE" , font='block', chr_ignore=True)
+print(Fore.LIGHTCYAN_EX)
+tprint("A" , font='block', chr_ignore=True)
+print(Fore.LIGHTMAGENTA_EX)
+tprint("NICE" , font='block', chr_ignore=True)
+print(Fore.LIGHTCYAN_EX)
+tprint("WEEK" , font='block', chr_ignore=True)
+tprint("END" , font='block', chr_ignore=True)
+print(Fore.LIGHTYELLOW_EX)
+print(decor("rand",both=True)*5)
+
+
+# print this if you want ^_^
+
+'''print(Fore.LIGHTRED_EX)
+print("\n\nAs you print this here you got this Question : \n")
+print(Fore.LIGHTMAGENTA_EX)
+tprint("\n\n Question 1 :\n\n",font="s-small")
+print(Fore.LIGHTMAGENTA_EX)
+tprint(" Why  did \nthe  bicycle\n fall  over? \n",font="s-small")
+print("| \nv\n| \nv\n| \nv\n| \nv")
+print(Fore.LIGHTYELLOW_EX)
+tprint(" \n\n\nBecause  it \nwas  two  tired ",font="s-small")
+print(Back.LIGHTBLUE_EX)
+print("Have a nice day .Bye~")
+aprint("happi231")
+init(autoreset=True)'''
+
+
