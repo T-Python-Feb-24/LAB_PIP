2ec3b8f8c9c7984b8990f3df19c9d4fe2de6295f
AsmaaFelemban <Asmaakf@hotmail.com>
2024-02-15 13:38:36 +0300

    Add more decorations

--- lab_pip.py
+++ lab_pip.py
@@ -1,12 +1,18 @@
 from art import *
-import colorama
+from colorama import Fore ,init
 
 # Create a new project to print Text Art in the terminal
 tprint("BELIEVE \nAND \nACHEIVE", font ="block")
 tprint(":HELLO", font = "sub-zero")
 
+
+
 # Bonus 
+
+init(autoreset=False)
+print( Fore.CYAN +"Asmaa")
+init(autoreset=True)
 tprint("WELCOME", space = 10)
-print( colorama.Fore.CYAN +"Asmaa")
+print('automatically back to default color again')
 
 
