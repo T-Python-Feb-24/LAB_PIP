4659056108561b9d45a89ef90a3bdda879f404c5
AsmaaFelemban <Asmaakf@hotmail.com>
2024-02-15 13:23:26 +0300

    Solved lab pip and bonus

---
+++ lab_pip.py
@@ -0,0 +1,12 @@
+from art import *
+import colorama
+
+# Create a new project to print Text Art in the terminal
+tprint("BELIEVE \nAND \nACHEIVE", font ="block")
+tprint(":HELLO", font = "sub-zero")
+
+# Bonus 
+tprint("WELCOME", space = 10)
+print( colorama.Fore.CYAN +"Asmaa")
+
+
