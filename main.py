ca97189d1309adcc91b3a77db68391ae2acd801e
Salemhu <salem.hussain.alsalem@gmail.com>
2024-02-15 11:52:36 +0300

    completed the lab.

---
+++ main.py
@@ -0,0 +1,13 @@
+from art import *
+from colorama import Fore, Back, Style
+FONT_COUNTER
+
+tprint("BELIEVE AND ACHEIVE",font="block")
+
+tprint("HELLO",font="sub-zero")
+
+tprint("I'm Salem",font="3d")
+
+tprint("Good morning",font="cola")
+
+print(Fore.RED +"Hello world")
