2d6a0eb343ca5139935755fb4091c7c5bab14160
Fai <faiabdulla123@gmail.com>
2024-02-15 14:39:26 +0300

    PIP

---
+++ PIP.py
@@ -0,0 +1,10 @@
+from art import*
+
+tprint("BELIEVE \nAND \nACHEIVE",font="block",chr_ignore=True)
+tprint("HELLO",font="sup -zero",chr_ignore=True)
+
+
+#Bounes
+from colorama import*
+print(Fore.GREEN+"FAI")
+
